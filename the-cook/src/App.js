b9ec5d4e245ae474464f1f4fa0f7f2ebc23fde15
Student_PSXGA136 <deablosf@hotmail.com>
2020-07-30 14:49:59 -0400

    one third done

--- the-cook/src/App.js
+++ the-cook/src/App.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import './App.css';
 import Ingredients from './components/Ingredients';
+import Names from './components/Names'
+import Buttons from './components/Buttons'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div>
       <header className="App-header">
-        <h1>The Hell Am I Cooking?!</h1>
+      <h1>The Hell Am I Cooking?!</h1>
         <h2>When takeout's a No Go</h2>
-        
-      </header>
-      <Ingredients />
-    </div>
+        <div className="linkin">
+          <div><Link to="/ingred">By Ingredients</Link></div>
+          <div><Link to="/name">By Name</Link></div>
+          <div><Link to="/buttons">See what Could Be</Link></div>
+        </div>
+
+        </header>
+        <div className="title">
+
+        </div>
+         {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL.  */}
+         <Switch>
+          <Route path="/name">
+            <Names />
+          </Route>
+          <Route path="/ingred">
+            <Ingredients />
+          </Route>
+          <Route path="/buttons">
+            <Buttons />
+          </Route>
+        </Switch> 
+      </div>
+    </Router>
+
   );
 }
 
